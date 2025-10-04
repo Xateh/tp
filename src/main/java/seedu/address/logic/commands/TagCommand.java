62c09ae79e89cec42f2e3ba844661b594c2a5539
mitchel <mitchel0303@gmail.com>
2025-10-05 01:09:30 +0800

    Add additive tagging

---
+++ src/main/java/seedu/address/logic/commands/TagCommand.java
@@ -0,0 +1,110 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Adds tag to existing person in address book.
+ */
+public class TagCommand extends Command {
+
+    public static final String COMMAND_WORD = "tag";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds tags to person identified "
+            + "by the index number used in the displayed person list. "
+            + "New tags will be added on top of existing tags.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[TAG NAME]...\n"
+            + "Example: " + COMMAND_WORD + " 1 beast friend";
+
+    public static final String MESSAGE_ADD_TAGS_SUCCESS = "Added tag to person: %1$s %2$s";
+    public static final String MESSAGE_NO_TAGS_PROVIDED = "At least one tag must be provided.";
+    public static final String MESSAGE_DUPLICATE_TAGS = "Some tags already exist for this person: %1$s";
+
+    private final Index index;
+    private final Set<Tag> tagsToAdd;
+
+    /**
+     * @param index of the person in the filtered person list to add tags
+     * @param tagsToAdd tags to add to the person
+     */
+    public TagCommand(Index index, Set<Tag> tagsToAdd) {
+        requireNonNull(index);
+        requireNonNull(tagsToAdd);
+
+        this.index = index;
+        this.tagsToAdd = new HashSet<>(tagsToAdd);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = createPersonWithAddedTags(personToEdit, tagsToAdd);
+
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        String tagsString = tagsToAdd.stream()
+                .map(tag -> tag.tagName)
+                .reduce((t1, t2) -> t1 + ", " + t2)
+                .orElse("");
+
+        return new CommandResult(String.format(MESSAGE_ADD_TAGS_SUCCESS,
+                Messages.format(editedPerson), tagsString));
+    }
+
+    /**
+     * Creates and returns a Person with tags added.
+     */
+    private static Person createPersonWithAddedTags(Person personToEdit, Set<Tag> tagsToAdd) {
+        assert personToEdit != null;
+
+        Set<Tag> updatedTags = new HashSet<>(personToEdit.getTags());
+        updatedTags.addAll(tagsToAdd);
+
+        return new Person(personToEdit.getName(), personToEdit.getPhone(),
+                personToEdit.getEmail(), personToEdit.getAddress(), updatedTags);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof TagCommand)) {
+            return false;
+        }
+
+        TagCommand otherTagCommand = (TagCommand) other;
+        return index.equals(otherTagCommand.index)
+                && tagsToAdd.equals(otherTagCommand.tagsToAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("index", index)
+                .add("tagsToAdd", tagsToAdd)
+                .toString();
+    }
+}
