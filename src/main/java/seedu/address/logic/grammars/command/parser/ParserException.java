b304b2cbec753f3746a9b6a82a8eeb8b8182f8c7
mattcce <ce.mattheus.cheong@gmail.com>
2025-10-05 17:54:33 +0800

    Migrate parser

---
+++ src/main/java/seedu/address/logic/grammars/command/parser/ParserException.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.grammars.command.parser;
+
+/**
+ * Exception for parsing errors (failures to apply production rules).
+ */
+public class ParserException extends Exception {
+    private final ParserError parserError;
+
+    ParserException(ParserError parserError) {
+        super(parserError.toString());
+        this.parserError = parserError;
+    }
+}
