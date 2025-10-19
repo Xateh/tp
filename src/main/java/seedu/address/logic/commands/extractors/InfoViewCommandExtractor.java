1e465f1be39d896fb77aa626f5c8bf30975e122e
mitchel <mitchel0303@gmail.com>
2025-10-19 20:04:26 +0800

    Add InfoViewCommand and extractor

---
+++ src/main/java/seedu/address/logic/commands/extractors/InfoViewCommandExtractor.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.commands.extractors;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.InfoViewCommand;
+import seedu.address.logic.commands.exceptions.ValidationException;
+import seedu.address.logic.grammars.command.BareCommand;
+
+/**
+ * Extractor that builds {@code InfoViewCommand}s
+ */
+public class InfoViewCommandExtractor {
+    public static final String MESSAGE_INDEX_UNSPECIFIED = "Index not specified.";
+    public static final String MESSAGE_INDEX_FAILED_TO_PARSE = "Invalid index: expected positive integer, got %1$s";
+    public static final String MESSAGE_INDEX_OUT_OF_RANGE = "Invalid index: expected positive integer, got %1$s";
+
+    private InfoViewCommandExtractor() {}
+
+    /**
+     * Extracts command parameters from the given Command object.
+     *
+     * @param bareCommand Command to extract parameters from.
+     * @return InfoViewCommand that can be executed.
+     * @throws ValidationException When the command parameters fail to validate.
+     */
+    public static InfoViewCommand extract(BareCommand bareCommand) throws ValidationException {
+        String[] params = bareCommand.getAllParameters();
+
+        if (params.length == 0) {
+            throw new ValidationException(MESSAGE_INDEX_UNSPECIFIED);
+        }
+
+        Index index;
+
+        try {
+            index = Index.fromOneBased(Integer.parseInt(params[0]));
+        } catch (NumberFormatException e) {
+            throw new ValidationException(String.format(MESSAGE_INDEX_FAILED_TO_PARSE, params[0]));
+        } catch (IndexOutOfBoundsException e) {
+            throw new ValidationException(String.format(MESSAGE_INDEX_OUT_OF_RANGE, params[0]));
+        }
+
+        return new InfoViewCommand(index);
+    }
+}
