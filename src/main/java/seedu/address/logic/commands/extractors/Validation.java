424879cc2a0720f076769a126a20f745713caa03
mattcce <ce.mattheus.cheong@gmail.com>
2025-10-21 12:21:13 +0800

    Refactor index validation out

---
+++ src/main/java/seedu/address/logic/commands/extractors/Validation.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.commands.extractors;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.ValidationException;
+
+/**
+ * Utility class for common extractions and validations used by multiple commands.
+ */
+public class Validation {
+    public static final String MESSAGE_INDEX_FAILED_TO_PARSE = "Invalid index: expected positive integer, got %1$s";
+    public static final String MESSAGE_INDEX_OUT_OF_RANGE = "Invalid index: expected positive integer, got %1$s";
+
+    private Validation() {}
+
+    /**
+     * Validate the {@code Index} input field type for commands.
+     * @param input String to validate.
+     * @return {@code Index} after validation.
+     * @throws ValidationException When the input fails to validate.
+     */
+    public static Index validateIndex(String input) throws ValidationException {
+        Index index;
+        try {
+            index = Index.fromOneBased(Integer.parseInt(input));
+        } catch (NumberFormatException e) {
+            // only thrown by Integer::parseInt
+            throw new ValidationException(String.format(MESSAGE_INDEX_FAILED_TO_PARSE, input));
+        } catch (IndexOutOfBoundsException e) {
+            // only thrown by Index::fromOneBased
+            throw new ValidationException(String.format(MESSAGE_INDEX_OUT_OF_RANGE, input));
+        }
+        return index;
+    }
+}
